1cb407731f1f71bb99a17e2e8514e32900c6ce7b
Lis0v <kokobolo@tutanota.com>
2023-06-18 16:57:40 +0200

    white line

--- line.c
+++ line.c
@@ -5,10 +5,12 @@
 
 #define x_res 	1280
 #define y_res 	800
+#define x_off	100
+#define y_off 	100
 #define col_n	4
 
 int main(){
-	int fd, x, y, x_size=255, y_size=255, x_off=0, y_off=0;
+	int fd, x, y, x_size=50, y_size=50;
 	unsigned char *fbmem;
 
 	fd = open("/dev/fb0", O_RDWR);
@@ -16,14 +18,15 @@ int main(){
 
 	fbmem += y_off * x_res * col_n + x_off * col_n;
 	
-	for(y = 0; y < y_size; y++){
-		for(x = 0; x < x_size; x++){
-			fbmem[x * col_n + 2 ] = 255; //R
-			fbmem[x * col_n + 1 ] = y;	 //G
-			fbmem[x * col_n 	] = x;	 //B
-		}
-		fbmem += x_res * col_n;
+	for(x = 0; x < x_size; x++){
+		fbmem[(x + x_res * y) * col_n + 0] = 255; 	//R
+		fbmem[(x + x_res * y) * col_n + 1] = 255;	//G
+		fbmem[(x + x_res * y) * col_n + 2] = 255;	//B
+		y++;
 	}
+	
+	fbmem += x_res * col_n;
+	
 	close(fd);
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,2 +1,4 @@
-grad:
+grad.:
 	cc grad.c -o grad.out
+line.:
+	cc line.c -o line.out
